01bc854f6127020bae5f8c5e70dd09ba5312360c
slimane rechdi <slimane.rechdi@gmail.com>
2020-10-15 11:47:28 -0400

    correction path in compile.bat

--- compile.bat
+++ compile.bat
@@ -1,6 +1,6 @@
-ipyc.exe /main:./bom/__main__.py ^
-./bom/api.py ^
-./bom/Interop.SolidEdge.dll ^
+ipyc.exe /main:__main__.py ^
+api.py ^
+Interop.SolidEdge.dll ^
 /embed ^
 /out:bom_recs_64x_0-0-0 ^
 /platform:x64 ^
